6742bf2da7088d046930b45c95c297e255b0dc01
GidaltiVelasquez <gidaltivelasquez38@gmail.com>
2024-02-06 22:30:56 -0500

    add dist

---
+++ dist/server/flight-server-css-manifest.js
@@ -0,0 +1,12 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\xampp\\htdocs\\landingmelatec\\Melatec\\app\\layout.tsx": [
+      "C:\\xampp\\htdocs\\landingmelatec\\Melatec\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\xampp\\htdocs\\landingmelatec\\Melatec\\app\\page": [
+      "C:\\xampp\\htdocs\\landingmelatec\\Melatec\\app\\globals.css"
+    ]
+  }
+}
